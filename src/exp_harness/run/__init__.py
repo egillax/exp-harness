c81f57a8d71fb5ecdb9dc323ddbcca2c08c177b1
egillax <egillax@gmail.com>
2026-03-03 22:05:35 +0100

    feat: add hydra multirun sweep execution

--- src/exp_harness/run/__init__.py
+++ src/exp_harness/run/__init__.py
@@ -2,16 +2,24 @@ from .api import (
     ComposeConfigError,
     OverrideParseError,
     RunResult,
+    SweepMemberResult,
+    SweepResult,
     compose_experiment_config,
+    expand_hydra_sweep_overrides,
     parse_set_overrides,
     run_experiment,
+    run_hydra_sweep,
 )
 
 __all__ = [
     "ComposeConfigError",
     "OverrideParseError",
     "RunResult",
+    "SweepMemberResult",
+    "SweepResult",
     "compose_experiment_config",
+    "expand_hydra_sweep_overrides",
     "parse_set_overrides",
     "run_experiment",
+    "run_hydra_sweep",
 ]
